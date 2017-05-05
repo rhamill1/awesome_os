8a36200748d90e83d068dd845b52ff61db0221ea
rhamill1 <rhamill11@gmail.com>
2017-05-05 16:31:38 -0700

    removes test comments and console.logs from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -10,19 +10,12 @@ $(document).ready(function(){
   $(window).scroll(function() {
     var windowScrollPosition = $(window).scrollTop();
     var opacityValue = (windowScrollPosition / 2.5)/100
-    // var marginTopString = marginTopValue + 'px'
-    console.log(opacityValue);
-
 
     if (windowScrollPosition < 10){
       $('header').css('background', 'rgba(255, 83, 21, .02)');
-      // $('.march_image').css('height', marginTopString);
     } else {
       $('header').css('background', 'rgba(255, 83, 21,' +  opacityValue + ')');
-      // $('.march_image').css('height', '167px');
     }
   });
-
-  console.log('document ready function successful!');
 
 });
